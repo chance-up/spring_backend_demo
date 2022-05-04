48ece1f80bd1215b7600c7767b080b53bd59f49e
chance-up <ccs6473@naver.com>
2022-05-05 07:42:42 +0900

    feat:repo 쿼리문 사용예제 추가

--- datajpa/src/main/java/com/example/datajpa/repository/CustomerRepository.java
+++ datajpa/src/main/java/com/example/datajpa/repository/CustomerRepository.java
@@ -1,9 +1,17 @@
 package com.example.datajpa.repository;
 
+import java.util.List;
+
 import com.example.datajpa.entity.Customer;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    @Query(value = "SELECT sys FROM ADM_SYS sys WHERE sys.nm = :nm OR sys.id = :id OR sys.tkcgrNm = :tkcgrNm")
+    // @Query(value = "SELECT sys FROM ADM_SYS sys WHERE sys.nm = :nm")
+    public List<Customer> search(@Param("nm") String nm, @Param("id") String id,
+            @Param("tkcgrNm") String tkcgrNm);
 
 }
